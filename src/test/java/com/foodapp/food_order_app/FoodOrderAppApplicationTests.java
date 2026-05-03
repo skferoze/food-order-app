187b01e60fc758962d2875da4976a97c5e152ad9
skferoze <sf73399@gmail.com>
2026-05-04 01:12:37 +0530

    Initial commit

---
+++ src/test/java/com/foodapp/food_order_app/FoodOrderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodapp.food_order_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodOrderAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
