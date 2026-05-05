557aded3e5b2e7b606d3785fcc06afb22a09a9a3
skferoze <sf73399@gmail.com>
2026-05-05 23:53:58 +0530

    enhanced some interface

--- src/main/java/com/foodapp/food_order_app/FoodOrderAppApplication.java
+++ src/main/java/com/foodapp/food_order_app/FoodOrderAppApplication.java
@@ -1,13 +1,18 @@
 package com.foodapp.food_order_app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FoodOrderAppApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(FoodOrderAppApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(FoodOrderAppApplication.class, args);
+		logger.info("Food Order Application started successfully");
 	}
 
 }
